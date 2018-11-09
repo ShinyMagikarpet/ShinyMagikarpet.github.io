985544908177bd18e2a7950bb0c84a1d0c13b7cd
ShinyMagikarpet <43708973+ShinyMagikarpet@users.noreply.github.com>
2018-11-09 00:37:00 -0500

    Hello

--- functions.js
+++ functions.js
@@ -28,9 +28,9 @@ function playSound(num) {
     var audio = new Audio();
 
     if (num == 0) {
-        audio.src = "summonSound.ogg";
+        audio.src = "jugglerPlay.ogg";
     } else {
-        audio.src = "attackSound.ogg";
+        audio.src = "jugglerAttack.ogg";
     }
     audio.volume = 0.2;
     audio.play();
